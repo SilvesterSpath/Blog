33b348a3ba0d75778f1556e522d0c34be74dad0c
SilvesterSpath <spath.sz@gmail.com>
2022-08-29 06:13:35 +0200

    moderation

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -9,7 +9,6 @@ const PostList = () => {
   const fetchPosts = async () => {
     const res = await axios.get('http://localhost:4002/posts');
     console.log(res.data);
-
     setPosts(res.data);
   };
 
