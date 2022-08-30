50cf8e1e69eb11e44f22f1aa20cbe4aa49a8c7f7
SilvesterSpath <spath.sz@gmail.com>
2022-08-30 07:40:11 +0200

    moderation frontend

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,6 +1,18 @@
 const CommentList = ({ comments }) => {
   const renderedComments = comments.map((item) => {
-    return <li key={item.id}>{item.content}</li>;
+    let content;
+
+    if (item.status === 'pending') {
+      content = 'Awaiting moderation..';
+    }
+    if (item.status === 'approved') {
+      content = item.content;
+    }
+    if (item.status === 'rejected') {
+      content = 'Has been rejected';
+    }
+
+    return <li key={item.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
