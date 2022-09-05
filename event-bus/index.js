b7df20cb7f1bfa33f28f3e30ea23d77d8d946748
SilvesterSpath <spath.sz@gmail.com>
2022-09-05 09:23:45 +0200

    event data store

--- event-bus/index.js
+++ event-bus/index.js
@@ -4,11 +4,14 @@ const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
-
 app.use(bodyParser.json());
+
+const events = [];
 
 app.post('/events', (req, res) => {
   const event = req.body;
+
+  events.push(event);
 
   axios
     .post('http://localhost:4000/events', event)
@@ -26,6 +29,11 @@ app.post('/events', (req, res) => {
   res.send({ status: 'OK' });
 });
 
+app.get('/events', (req, res) => {
+  res.send(events);
+});
+
 app.listen(4005, () => {
   console.log('Event-bus on 4005');
+  console.log(events);
 });
--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(cors());
@@ -22,13 +23,7 @@ const posts = {};
     },
   }; */
 
-app.get('/posts', (req, res) => {
-  res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-  const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
   if (type === 'PostCreated') {
     const { id, title } = data;
 
@@ -49,12 +44,29 @@ app.post('/events', (req, res) => {
     comment.status = status;
     comment.content = content;
   }
+};
 
-  console.log(posts);
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  handleEvent(type, data);
 
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log('Query on 4002');
+
+  const res = await axios.get('http://localhost:4005/events');
+
+  console.log(res.data);
+
+  for (let item of res.data) {
+    console.log('Processing event: ', item.type);
+    handleEvent(item.type, item.data);
+  }
 });
